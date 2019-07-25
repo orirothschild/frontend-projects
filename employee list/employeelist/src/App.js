67c76d568083dd929afdafb1dedf1f58e1f4d636
orirothschild <rotchildori@gmail.com>
2019-07-25 16:39:16 +0300

    list variable exists

--- employee list/employeelist/src/App.js
+++ employee list/employeelist/src/App.js
@@ -6,9 +6,45 @@ import {List} from './Components/list'
 
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        employees: [
+     
+            {
+              avatar: 'https://image.shutterstock.com/z/stock-photo-experienced-businesswoman-reading-publication-via-laptop-computer-making-research-for-planning-1051137005.jpg',
+              date: 'Joined in 2013',
+              header: 'Helen',
+              description: 'Primary Contact',
+              isWorking: false
+
+            },
+            {
+              avatar: '/images/avatar/large/matthew.png',
+              date: 'Joined in 2013',
+              header: 'Matthew',
+              description: 'Primary Contact',
+              isWorking: false
+            },
+            {
+              avatar: '/images/avatar/large/molly.png',
+              date: 'Joined in 2013',
+              header: 'Molly',
+              description: 'Primary Contact',
+              isWorking: true
+            },
+        ]
+    }
+  }       
   render () {
   return (
-    <List/>
+
+
+
+    <List employees={this.state.employees}>
+
+    </List>
+
   );
   }
 }
--- employee list/employeelist/src/Components/list.js
+++ employee list/employeelist/src/Components/list.js
@@ -1,15 +1,60 @@
-import React from 'react'
-import { Image, Reveal } from 'semantic-ui-react'
 
-const listView = () => (
-  <Reveal animated='rotate'>
-    <Reveal.Content visible>
-      <Image circular size='small' src='/images/wireframe/square-image.png' />
-    </Reveal.Content>
-    <Reveal.Content hidden>
-      <Image circular size='small' src='/images/avatar/large/stevie.jpg' />
-    </Reveal.Content>
-  </Reveal>
-)
+import React, { Component, Fragment } from 'react'
+import _ from 'lodash'
+import { Button, Card, Divider, Image, Placeholder, Reveal  } from 'semantic-ui-react'
 
-export {listView as List};
+
+class employeeList extends Component{
+    
+    employeeFragmentation = (employee) => {
+      return (
+        <Fragment>
+          <Card.Group doubling itemsPerRow={3} stackable>
+              <Card key={employee.header}>
+                <Image src={employee.avatar} />
+                <Card.Content>
+                <Fragment>
+                <Card.Header>{employee.header}</Card.Header>
+                <Card.Meta>{employee.date}</Card.Meta>
+                <Card.Description>{employee.description}</Card.Description>
+                </Fragment>
+                  
+                </Card.Content>
+  
+                <Card.Content extra>
+                  <Button  primary>
+                    Add
+                  </Button>
+                  <Button>Delete</Button>
+                </Card.Content>
+              </Card>
+          </Card.Group>
+        </Fragment>
+      )
+    }
+  
+    listView = () => (
+        
+        <Fragment>
+         <Divider />
+        {_.map(this.props.employees,employee =>(
+      <Reveal animated='rotate'>
+        <Reveal.Content visible>
+        <Image circular size='small' src='https://image.shutterstock.com/image-vector/black-abstract-halftone-logo-design-600w-175771670.jpg' />
+        </Reveal.Content>
+        <Reveal.Content hidden>
+       
+          {this.employeeFragmentation(employee)}
+        </Reveal.Content>
+      </Reveal>
+        ))}
+      </Fragment>
+    )
+
+    render(){
+        return this.listView();
+    }
+}
+
+
+export {employeeList as List};
