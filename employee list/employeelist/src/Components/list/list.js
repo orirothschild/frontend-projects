c0cc093516bf9ce64143a6fdf80d7e5339e02917
orirothschild <rotchildori@gmail.com>
2019-07-29 20:46:30 +0300

    add buttom succsess

--- employee list/employeelist/src/Components/list/list.js
+++ employee list/employeelist/src/Components/list/list.js
@@ -2,16 +2,46 @@
 import React, { Component} from 'react'
 import _ from 'lodash'
 import './list.css'
-import {observer} from 'mobx-react'; 
+import {observer} from 'mobx-react';
+import { Form, Button } from 'semantic-ui-react';
 import EmployeeItem from '../EmployeeItem/EmployeeItem'
+import { observable, action } from 'mobx';
+const options = [
+    { key: 'm', text: 'Male', value: 'male' },
+    { key: 'f', text: 'Female', value: 'female' },
+    { key: 'o', text: 'Other', value: 'other' },
+  ]
 
 @observer
 class employeeList extends Component{
-  handleCreateNewEmployee(event) {
-    
+    constructor(props){
+        super(props);
+        this.handleAdding =this.props.handleAdding.bind(this);
+    }
+  handleCreateNewEmployee = () => {
+      if(this.props.isAdding){
+    return (
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='First name' placeholder='First name' />
+          <Form.Input fluid label='Last name' placeholder='Last name' />
+          <Form.Select fluid label='Gender' options={options} placeholder='Gender' />
+        </Form.Group>
+        <Form.TextArea label='About' placeholder='Tell us more about you...' />
+        <Form.Checkbox label='I agree to the Terms and Conditions' />
+        <Form.Button onClick={this.handleAdding}>Submit</Form.Button>
+      </Form>
+    )
+  }
 }
     render(){
-        return _.map(this.props.employees ,(employee, index) => <EmployeeItem key={index} employee={employee} deleteEmployee={this.props.deleteEmployee}/>);
+        return(
+            <div>
+            {_.map(this.props.employees ,(employee, index) => <EmployeeItem key={index} employee={employee} deleteEmployee={this.props.deleteEmployee} AddEmployee={this.handleCreateNewEmployee}/>)};
+            {!this.props.isAdding && <Button primary onClick={this.handleAdding}>Add</Button>}
+            {this.handleCreateNewEmployee()}
+            </div>
+            )
     }
 }
 
